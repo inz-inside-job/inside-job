088186cb77e203deff142f30004d00da909057ae
Gintaras <gintaras.gaucys1@gmail.com>
2025-03-23 01:40:11 +0000

    added jobs filter component

--- resources/js/pages/jobs.tsx
+++ resources/js/pages/jobs.tsx
@@ -1,3 +1,4 @@
+import { JobFilters } from '@/components/jobs/jobs-filters';
 import { JobsHero } from '@/components/jobs/jobs-hero';
 import { Head } from '@inertiajs/react';
 
@@ -8,7 +9,9 @@ export default function CompaniesPage() {
             <JobsHero />
             <div className="container mx-auto w-full px-4 py-8">
                 <div className="flex flex-col gap-6 lg:flex-row lg:gap-8">
-                    <aside className="w-full shrink-0 lg:w-80 xl:w-96"></aside>
+                    <aside className="w-full shrink-0 lg:w-80 xl:w-96">
+                        <JobFilters />
+                    </aside>
                     <div className="flex-1"></div>
                 </div>
             </div>
